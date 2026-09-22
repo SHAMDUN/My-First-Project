a3d80aaae80b59feafce8e91fce7b61928b7414a
SHAMDUN <beat.market.office@gmail.com>
2026-09-22 14:04:25 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // ==========================================
 
 // نسخه خودکار: هر بار که فایل تغییر کنه، نسخه جدید میاد
-const VERSION = 'v2.0.10';
+const VERSION = 'v2.0.0';
 const CACHE_NAME = `shamdun-${VERSION}`;
 
 const urlsToCache = [
