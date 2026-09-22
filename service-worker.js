f05409a22e78e924088a2783f3b196582b22478b
SHAMDUN <beat.market.office@gmail.com>
2026-09-22 09:45:59 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 // Service Worker - آکادمی شمعدون
 // ==========================================
 
-const CACHE_NAME = 'shamdun-v58'; // ← این عدد رو بعد از هر آپدیت عوض کن
+const CACHE_NAME = 'shamdun-v59'; // ← این عدد رو بعد از هر آپدیت عوض کن
 
 const urlsToCache = [
     '/My-First-Project/',
